007fd05922526e97e99ec11da20bcad56658f4a6
conquest-bishop <conquestacb@gmail.com>
2023-08-25 17:19:32 -0700

    added counter to director dash

--- controllers/dashRoute.js
+++ controllers/dashRoute.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const Park =  require('../models/parkModel');
+const Clinic =  require('../models/clinicModel');
+
+
 const {ensureLoggedIn} = require('connect-ensure-login');
 
 
 
-router.get('/directordash', ensureLoggedIn('/api/login'), (req, res)=>{
+router.get('/directordash', ensureLoggedIn('/api/login'), async(req, res)=>{
     req.session.user = req.user;
+    let count = await Park.countDocuments(); 
+    console.log('Total documents in Park collection:', count);
+    let count2 = await Clinic.countDocuments(); 
+    console.log('Total documents in clinic collection:', count2);
+
+
+
     // let loggedInUser = req.session.user.fullname
-    res.render('director_.pug')
+    res.render('director_.pug', {count, count2})
 })
 
 router.get('/managerdash', ensureLoggedIn('/api/login'), (req, res)=>{
